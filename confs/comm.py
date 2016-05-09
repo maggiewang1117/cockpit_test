9e14afe0cb4f1a1a6b1094611b7cb54cfd278c49
maggiewang <huiwa@redhat.com>
2016-05-09 11:11:19 +0800

    finished the case to test login

--- confs/comm.py
+++ confs/comm.py
@@ -7,3 +7,5 @@ ipaddr = "10.66.10.187"
 username = "root"
 
 passwd = "redhat"
+
+rhevh_version_36 =  "Red Hat Enterprise Virtualization Hypervisor 3.6"
--- testcases/login/test_login.py
+++ testcases/login/test_login.py
@@ -18,6 +18,7 @@ class TestLogin(object):
         self.ssh_login = general.EstabSSHConnect(
             self.ipaddr, self.username, self.passwd)
         self.ssh_conn = self.ssh_login.ssh_connect()
+        self.rhevh_version = comm.rhevh_version_36
         self.result = 0
 
     def init_webdirver(self):
@@ -36,6 +37,21 @@ class TestLogin(object):
 
         login_btn = web_driver.find_element_by_id("login-button")
         login_btn.send_keys(Keys.ENTER)
+        result = self.check_login_status(web_driver)
+        return result
+
+    def check_login_status(self, web_driver):
+    	web_driver = web_driver
+    	web_driver.implicitly_wait(10)
+    	current_user = web_driver.find_element_by_id("content-user-name").text
+
+    	if current_user.strip() == self.username:
+    		self.logger.info("Login with '%s' succeed!" % self.username)
+    		self.result += 0
+    	else:
+    		self.logger.error("Login with '%s' failed!" % self.username)
+    		self.result += 1
+    	return self.result
 
     def get_all_infos_in_server(self):
         stdin, stdout, stderr = self.ssh_conn.exec_command("hostname")
@@ -56,12 +72,32 @@ class TestLogin(object):
             self.logger.error("Login page shows '%s'" % server_name)
             self.logger.error("Hostname in host is '%s'" % s_hostname)
             self.result += 1
-        print self.result
+        return self.result
+
+    def check_version_in_login_page(self, web_driver, verion_name):
+    	web_driver = web_driver
+    	version_in_login = str(web_driver.find_element_by_id("brand").text)
+    	verion_name = verion_name.upper()
+    	f = re.compile(verion_name)
+
+    	if f.findall(version_in_login):
+    		self.logger.info("Login Page shows '%s'" % version_in_login)
+    		self.logger.info("Version should be '%s'" % verion_name)
+    		self.result += 0
+    	else:
+    		self.logger.error("Login Page shows '%s'" % version_in_login)
+    		self.logger.error("Version should be '%s'" % verion_name)
+    		self.result += 1
+    	return self.result
+
 
     def run(self):
         f_driver = self.init_webdirver()
-        self.check_server_name_in_login_page(f_driver)
-        self.login(f_driver)
+        total_result = self.check_version_in_login_page(f_driver, self.rhevh_version)
+        total_result += self.check_server_name_in_login_page(f_driver)
+        total_result += self.login(f_driver)
+        f_driver.close()
+        return total_result
 
 
 if __name__ == "__main__":
